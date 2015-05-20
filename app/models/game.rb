d12a26fec6d078a6298aa5acf2923449f55d6097
agufranov <albert.gufranov@gmail.com>
2015-05-20 12:02:00 +0500

    [fix] dealer doesnt move when no cards

--- app/models/game.rb
+++ app/models/game.rb
@@ -61,12 +61,14 @@ class Game
   end
 
   def notify_player_finished
+    # TODO когда сплит, и в одну руку кладем последнюю карту, во второй надо как-то отключить шаги
+    # например, уведомлять игру после шага, чтобы она проверила количество карт
     dh, ph = dealer.hand, player.hand
     if player.hands.all? &:busted?
       dealer.hand.stand! unless dealer.hand.standing?
     end
 
-    while dh.playing? do
+    while dh.playing? and shoes.any? do
       dh.hit!
     end
 
